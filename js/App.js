0bf85d06dfb8d5b972f658e13492cceec851cc5c
aroerick <a.roerick@gmail.com>
2018-09-18 16:25:11 -0700

    Add user context

--- js/App.js
+++ js/App.js
@@ -6,6 +6,7 @@ import createRootNavigator from './navigation/RootStackNavigation';
 import { getUser } from './config/models';
 import { UserProvider } from './context/UserContext';
 import ItemsProvider from './context/ItemsProvider';
+import { UserProvider } from './context/UserContext';
 
 export default class App extends Component {
   constructor(props) {
